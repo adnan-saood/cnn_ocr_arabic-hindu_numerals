f8c79a112afb476495c3cb621f11917cfe49a534
Adnan Saood <adnan.saood@outlook.com>
2021-01-24 15:19:30 +0200

    Clean Submission

--- train_cnn.m
+++ train_cnn.m
@@ -43,39 +43,4 @@ layers = [
     reluLayer
     
     maxPooling2dLayer(2,'Stride',2)
-    
-    convolution2dLayer(3,32,'Padding','same')
-    batchNormalizationLayer
-    reluLayer
-    
-     maxPooling2dLayer(2,'Stride',2)
-     
-    convolution2dLayer(3,64,'Padding','same')
-    batchNormalizationLayer
-    reluLayer
-      
-    
-    fullyConnectedLayer(10)
-    softmaxLayer
-    classificationLayer];
-
-
-%%
-options = trainingOptions('adam', ...
-    'InitialLearnRate',0.005, ...
-    'MaxEpochs',60, ...
-    'Shuffle','every-epoch', ...
-    'ValidationData',imdsValidation, ...
-    'ValidationFrequency',4, ...
-    'Verbose',false, ...
-    'Plots','training-progress');
-
-
-%%
-[net,data] = trainNetwork(imdsTrain,layers,options);
-%%
-figure;
-
-YPred = classify(net,imdsValidation);
-YValidation = imdsValidation.Labels;
-confusionchart(YPred,YValidation);
+    ر 
